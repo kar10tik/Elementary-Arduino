e23290ed7ce96d9c18d1b8485687c0d6f91b6908
Kartik Sahajpal <kartik_sahajpal@sindia.co.in>
2023-03-08 17:09:21 +0530

    Added connection schematic to stepper motor L298N code

--- stepper_L298_control/stepper_L298_control.ino
+++ stepper_L298_control/stepper_L298_control.ino
@@ -1,6 +1,20 @@
 // Include the Arduino Stepper Library
 #include <Stepper.h>
-
+/*
+Pin Connections
+  Stepper to L298N
+    BLUE to L298N OUT1
+    YELLOW to L298N OUT2
+    PINK to L298N OUT3
+    ORANGE to L298N OUT4
+  L298N to Arduino
+    IN1 to D2
+    IN2 to D3
+    IN2 to D4
+    IN2 to D5
+    VCC to 5V
+    GND to GND  
+*/
 // Number of steps per output rotation
 const int stepsPerRevolution = 300;
 
